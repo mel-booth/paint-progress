a3f62e4a838b46257a5930bf2c72a1e386aa5eaa
Apprentice <apprentice@StMac0-17.local>
2016-11-11 12:27:09 +1300

    it still kinda works

--- db/getEntries.js
+++ db/getEntries.js
@@ -21,7 +21,7 @@ function setEntry(obj) {
 
 function getEntriesByProjectId(id) {
   return knex('entries')
-    .join('projects', 'project_id', id)
+    .join('projects', 'entries.project_id', id)
     .join('users', 'user_id', 'users.id')
     .orderBy('entries.id', 'desc')
 }
