37e2567b6ee689f9a9033ee359bd54dc8befebd8
Apprentice <apprentice@StMac0-17.local>
2016-11-10 14:48:39 +1300

    update newEntry

--- db/getEntries.js
+++ db/getEntries.js
@@ -3,9 +3,15 @@ var knexConfig = require('../knexfile')[process.env.NODE_ENV || "development"]
 var knex = Knex(knexConfig)
 
 module.exports = {
-  getEntries
+  getEntries,
+  setEntry
 }
 
 function getEntries() {
   return knex('entries')
+}
+
+function setEntry(obj) {
+  return knex('entries').insert({entryTitle: obj.entryTitle, date: obj.date, image: obj.image})
+
 }
